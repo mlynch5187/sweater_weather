a1570bc38badbc1eeb458507e898713875710a98
Alex Lynch <mlynch5187@gmail.com>
2020-09-20 13:23:46 -0600

    Bolster pexelS_service_spec; address misspelled hash key in ForecastController index

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'MapQuest Geocoding Service' do
   it 'Retrieves latitude and longitude of an address' do
 
     response = MapQuestService.new.longitude_latitude("denver, co")
+
     expect(response[:lat]).to eq(39.738453)
     expect(response[:lng]).to eq(-104.984853)
   end
--- spec/services/pexels_service_spec.rb
+++ spec/services/pexels_service_spec.rb
@@ -4,5 +4,9 @@ RSpec.describe 'Pexels Service' do
   it 'Retrieves an images for location' do
     location = 'denver, co'
     response = PexelsService.new.images(location)
+
+    expect(response[:photographer]).to_not be_nil
+    expect(response[:photographer_url]).to_not be_nil
+    expect(response[:src][:original]).to_not be_nil
   end
 end
