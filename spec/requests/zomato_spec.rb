a81b09e08551b891f4b1040c4546ea4748cfdf67
Alex Lynch <mlynch5187@gmail.com>
2020-09-22 13:36:31 -0600

    Remove commented out line

--- spec/requests/zomato_spec.rb
+++ spec/requests/zomato_spec.rb
@@ -9,16 +9,13 @@ describe "Zomato API" do
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(json[:data][:attributes][:forecast]).to eq('clear sky')
-    expect(json[:data][:attributes][:current_temp]).to eq(301.21)
     expect(json[:data][:attributes][:travel_time_minutes]).to eq("41")
     expect(json[:data][:attributes][:travel_time_hours]).to eq("00")
     expect(json[:data][:attributes][:restaurant_name]).to eq("Bingo Burger")
 
     expect(json[:data][:attributes][:restaurant_location][:city]).to eq('Pueblo')
-    expect(json[:data][:attributes][:restaurant_location][:city_id]).to eq('535')
+    expect(json[:data][:attributes][:restaurant_location][:city_id]).to eq(535)
     expect(json[:data][:attributes][:restaurant_location][:zipcode]).to eq('81003')
     expect(json[:data][:attributes][:restaurant_location][:address]).to eq('101 Central Plaza 81003')
-
-    # expect(response).to
   end
 end
