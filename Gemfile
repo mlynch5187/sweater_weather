96bb7b11d8b1d5afd29cff1cd4a6c3be118d0f55
Alex Lynch <mlynch5187@gmail.com>
2020-09-22 13:46:47 -0600

    Add Faker, Simplecov to Gemfile; create user factory

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 4.0.1'
   gem 'pry'
+  gem 'faker'
+  gem 'simplecov'
 end
 
 group :development do
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,17 @@
 class Api::V1::UsersController < ApplicationController
-  def create; end
+  def create
+    user = User.new(user_params)
+    if user.save
+      user.create_key
+      render json: UserSerializer.new(user)
+    else
+      render json: ErrorSerializer.new(user.errors)
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 end
