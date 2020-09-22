d76bbbfcd5f2f8917edf98e36fedf0bd28331792
Alex Lynch <mlynch5187@gmail.com>
2020-09-22 10:41:49 -0600

    Retrieve restaurant json

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -11,7 +11,7 @@ class BaseService
 
   def zomato_conn(uri)
     Faraday.new(uri) do |f|
-      f.headers['user-key'] = ENV['ZOMATO_API']
+      f.headers['user-key'] = '0b3582e338e764a9df0c0f8111a59338'
     end
   end
 
--- app/services/zomato_service.rb
+++ app/services/zomato_service.rb
@@ -5,10 +5,22 @@ class ZomatoService < BaseService
   end
 
   def city_id
-      response = zomato_conn('https://developers.zomato.com/api/v2.1').get('/cities') do |c|
-        c.params['q'] = @end_city
-        c.params['count'] = 1
+      response = zomato_conn('https://developers.zomato.com').get('/api/v2.1/cities') do |f|
+        f.params[:q] = @end_city
+        f.params[:count] = 1
       end
-      json(response)
+      json(response)[:location_suggestions][0][:id]
     end
+
+  def restaurant
+    location = city_id
+    response = zomato_conn('https://developers.zomato.com').get('/api/v2.1/search') do |f|
+    f.params[:entity_id] = location
+    f.params[:entity_type] = 'city'
+    f.params[:count] = 1
+    f.params[:radius] = 5
+    end
+    require "pry"; binding.pry
+   json(response)[:restaurants][0][:restaurant]
+  end
 end
