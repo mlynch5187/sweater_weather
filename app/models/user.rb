885fc8adf063460595151efa791802b5fd57d728
Alex Lynch <mlynch5187@gmail.com>
2020-09-22 14:03:29 -0600

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
-  validates_presence_of :api_key, require: true
-  validates :password_digest, presence: true, confirmation: { case_sensitive: true}
+  validates :password, presence: true, confirmation: { case_sensitive: true}
+  validates :api_key, uniqueness: true
+
   has_secure_password
+
+  def create_key
+    self.update(api_key: ApiGenerator.call)
+  end
 end
