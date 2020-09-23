08f3637d50197b70f747b259d364aa814d9ff769
Alex Lynch <mlynch5187@gmail.com>
2020-09-22 23:18:04 -0600

    Bolster forecast_service_spec

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -15,19 +15,15 @@ class Forecast
               :hourly_feels_like,
               :hourly_humidity,
               :hourly_visibility,
-              :hourly_uv_index,
-              :hourly_sunrise,
-              :hourly_sunset,
               :hourly_summary,
+              :hourly_icon,
               :daily_temp,
               :daily_feels_like,
               :daily_humidity,
-              :daily_visibility,
               :daily_uv_index,
               :daily_sunrise,
               :daily_sunset,
               :daily_summary
-
 
   def initialize(weather_forecast)
     @id = 1
@@ -38,25 +34,24 @@ class Forecast
     @current_feels_like = weather_forecast[:current][:feels_like]
     @current_humidity = weather_forecast[:current][:humidity]
     @current_visibility = weather_forecast[:current][:visibility]
-    @current_uv_index = weather_forecast[:current][:uv_index]
+    @current_uv_index = weather_forecast[:current][:uvi]
     @current_sunrise = weather_forecast[:current][:sunrise]
     @current_sunset = weather_forecast[:current][:sunset]
     @current_summary = weather_forecast[:current][:weather][0][:description]
-    @hourly_temp = weather_forecast[:hourly][:temp]
-    @hourly_feels_like = weather_forecast[:hourly][:feels_like]
-    @hourly_humidity = weather_forecast[:hourly][:humidity]
-    @hourly_visibility = weather_forecast[:hourly][:visibility]
-    @hourly_uv_index = weather_forecast[:hourly][:uv_index]
-    @hourly_sunrise = weather_forecast[:hourly][:sunrise]
-    @hourly_sunset = weather_forecast[:hourly][:sunset]
-    @hourly_summary = weather_forecast[:hourly][:weather][0][:description]
-    @daily_temp = weather_forecast[:daily][:temp]
-    @daily_feels_like = weather_forecast[:daily][:feels_like]
-    @daily_humidity = weather_forecast[:daily][:humidity]
-    @daily_visibility = weather_forecast[:daily][:visibility]
-    @daily_uv_index = weather_forecast[:daily][:uv_index]
-    @daily_sunrise = weather_forecast[:daily][:sunrise]
-    @daily_sunset = weather_forecast[:daily][:sunset]
-    @daily_summary = weather_forecast[:daily][:weather][0][:description]
+    @current_icon = weather_forecast[:current][:weather][0][:icon]
+    @hourly_temp = weather_forecast[:hourly][0][:temp]
+    @hourly_feels_like = weather_forecast[:hourly][0][:feels_like]
+    @hourly_humidity = weather_forecast[:hourly][0][:humidity]
+    @hourly_visibility = weather_forecast[:hourly][0][:visibility]
+    @hourly_summary = weather_forecast[:hourly][0][:weather][0][:description]
+    @hourly_icon = weather_forecast[:hourly][0][:weather][0][:icon]
+    @daily_temp = weather_forecast[:daily][0][:temp]
+    @daily_feels_like = weather_forecast[:daily][0][:feels_like]
+    @daily_humidity = weather_forecast[:daily][0][:humidity]
+    @daily_uv_index = weather_forecast[:daily][0][:uvi]
+    @daily_sunrise = weather_forecast[:daily][0][:sunrise]
+    @daily_sunset = weather_forecast[:daily][0][:sunset]
+    @daily_summary = weather_forecast[:daily][0][:weather][0][:description]
+    @daily_icon = weather_forecast[:daily][0][:weather][0][:icon]
   end
 end
